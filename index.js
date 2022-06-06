10d0f26e6e464541d4e7e1c4037a8eb47d386f5f
Ivanakowalczuk <ivanakowalczuk@gmail.com>
2022-06-06 00:13:59 -0300

    POO

---
+++ index.js
@@ -0,0 +1,33 @@
+// Crea un nuevo fichero JS que contenga las siguientes líneas
+
+// - Una clase llamada "Estudiante" que tenga:
+
+// - Una variable llamada nombre
+
+// - Otra variable lista llamada asignaturas con 3 asignaturas: Javascript, HTML, CSS
+
+// - Un método "obtenDatos" que devuelva un objeto con las propiedades nombre y asignaturas
+
+// - Crea una nueva instancia de "Estudiante"
+
+// - Haz la llamada al método obtenDatos
+
+
+class Estudiante  {
+   
+    constructor(nombre, asignaturas){
+        this.nombre= nombre,
+        this.asignaturas= asignaturas,
+        asignaturas = ["Javascript", "HTML", "CSS"]
+    }
+
+    obtenDatos(){
+      return `Mi nombre es ${this.nombre} y estudio  ${this.asignaturas}`
+        
+    }
+
+}
+
+const nuevo_estudiante = new Estudiante ("Juan", ["Javascript", "HTML", "CSS" ])
+console.log(nuevo_estudiante)
+console.log(nuevo_estudiante.obtenDatos())
